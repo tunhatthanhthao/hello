b369b2aa71fff96f769f14736dd609ec8e74f114
thtu <ttu@huskers.unl.edu>
2020-08-10 07:07:38 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+
+*Author: Tu Nhat Thanh Thao
+*Date : 2020
+* 
+* A simple hello world program
+*/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char ** argv) {
+        printf("Tu Nhat Thanh Thao!\n");
+        printf("Computer Science\n");
+        return 0;
+}
